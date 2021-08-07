39c8416948aabf0a886a46a155f116e42cd4ee7e
Ege Burak Özpınar <egeburak.ozpinar@trendyol.com>
2021-08-07 19:46:13 +0300

    updated

--- app.py
+++ app.py
@@ -2,6 +2,21 @@ import requests
 
 PROMETHEUS = 'http://prometheus-server:80' #conf dosyasından çekilecek.
 
+# TODO: kube_pod_container_resource_requests conf dosyasından çekilecek.
+# TODO: cluster name (ör: p-checkout-p1-2moon) conf'a eklenecek ileride gitlab pipeline'ından verilecek. Amaç: slack'e mesaj gönderirken kullanılacak.
+# TODO: slack entegrasyonu
+
+class obj:
+  def __init__(self, pod, cluster, cpu_req, cpu_limit, cpu_usage, mem_req, mem_limit, mem_usage):
+      self.pod = pod
+      self.cluster = cluster
+      self.cpu_req = cpu_req
+      self.cpu_limit = cpu_limit
+      self.cpu_usage = cpu_usage
+      self.mem_req = mem_req
+      self.mem_limit = mem_limit
+      self.mem_usage = mem_usage
+
 
 def get_data(prometheus_url, query):
   response =requests.get(prometheus_url + '/api/v1/query', params={'query': query})

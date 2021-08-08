22f58ccc939deea5aaa41da7d7346475b3dbe2fe
Mehmet Emin Bora <mehmet.bora@trendyol.com>
2021-08-08 23:58:06 +0300

    upgraded

--- app.py
+++ app.py
@@ -11,13 +11,15 @@ pod_list = []
 
 prometheus_api.get_requests_from_prometheus(pod_list)
 prometheus_api.get_limits_from_prometheus(pod_list)
+prometheus_api.get_cpu_usage_from_prometheus(pod_list)
 
-
+"""
 for pod in pod_list:
   print(pod.pod_name)
   print(pod.cpu_req + " => " + pod.cpu_limit)
   print(pod.mem_req + " => " + pod.mem_limit)
   print("-----")
+"""
 
 
 '''
--- prometheus_api.py
+++ prometheus_api.py
@@ -8,6 +8,7 @@ def get_data(prometheus_url, query):
   return results
 
 
+# Bu ve altındaki fonksiyonları birleştirebiliriz.
 def get_requests_from_prometheus(pod_list):
     metrics = get_data('http://prometheus-server:80', 'kube_pod_container_resource_requests')
 
@@ -35,3 +36,17 @@ def get_limits_from_prometheus(pod_list):
 
         pod_list[pod_index].set_pod_limits(metric['metric']['resource'], metric['value'][1])
 
+def get_cpu_usage_from_prometheus(pod_list):
+    metrics = get_data('http://prometheus-server:80', 'sum(irate(container_cpu_usage_seconds_total{container!=""}[2m]))by(node,pod)')
+    print(metrics)
+"""
+    for metric in metrics:
+        pod_index = pod_list_funcs.get_pod_index(pod_list, metric['metric']['pod'])
+        
+        if pod_index == -1:
+            pod = pod_class.Pod(metric['metric']['pod'])
+            pod_list.append(pod)
+            pod_index = len(pod_list) - 1
+
+        pod_list[pod_index].set_pod_limits(metric['metric']['resource'], metric['value'][1])
+"""

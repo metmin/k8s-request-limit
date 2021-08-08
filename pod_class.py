45f817ae56787f87f2638cc92fc6e4b5097eacda
Mehmet Emin Bora <mehmet.bora@trendyol.com>
2021-08-08 23:38:42 +0300

    upgraded

--- pod_class.py
+++ pod_class.py
@@ -1,8 +1,16 @@
-class Pods:
+class Pod:
+  cpu_req = ""
+  cpu_limit = ""
+  cpu_usage = ""
+  mem_req = ""
+  mem_limit = ""
+  mem_usage = ""
+  
   def __init__(self, pod_name, cluster = 'deneme'):
   #def __init__(self, pod, cluster, cpu_req, cpu_limit, cpu_usage, mem_req, mem_limit, mem_usage):
       self.pod_name = pod_name
       self.cluster = cluster
+      
       '''
       self.cpu_req = cpu_req
       self.cpu_limit = cpu_limit

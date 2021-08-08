fbf742d602b8004d2762b83dc3a2f7f68bdb836c
Mehmet Emin Bora <mehmet.bora@trendyol.com>
2021-08-09 00:32:13 +0300

    upgraded

--- prometheus_api.py
+++ prometheus_api.py
@@ -52,12 +52,11 @@ def get_cpu_usage_from_prometheus(pod_list):
 
 
 def get_memory_usage_from_prometheus(pod_list):
-    metrics = get_data('http://prometheus-server:80', 'avg(container_memory_working_set_bytes{image=""})by(pod)')
+    metrics = get_data('http://prometheus-server:80', 'avg(container_memory_working_set_bytes{pod!="",image=""})by(pod)')
 
     for metric in metrics:
         print(metric)
 
-        """
         pod_index = pod_list_funcs.get_pod_index(pod_list, metric['metric']['pod'])
         if pod_index == -1:
             pod = pod_class.Pod(metric['metric']['pod'])
@@ -65,4 +64,4 @@ def get_memory_usage_from_prometheus(pod_list):
             pod_index = len(pod_list) - 1
 
         pod_list[pod_index].mem_usage = metric['value'][1]
-        """
+        

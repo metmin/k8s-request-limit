e4482c867eda0022321dced10ed30e4caa6cd535
Mehmet Emin Bora <mehmet.bora@trendyol.com>
2021-08-08 23:27:02 +0300

    upgraded

--- prometheus_api.py
+++ prometheus_api.py
@@ -21,3 +21,18 @@ def get_requests_from_prometheus(pod_list):
             pod_index = len(pod_list) - 1
 
         pod_list[pod_index].set_pod_requests(metric['metric']['resource'], metric['value'][1])
+
+def get_limits_from_prometheus(pod_list):
+    metrics = get_data('http://prometheus-server:80', 'kube_pod_container_resource_limits')
+    print(metrics)
+'''
+    for metric in metrics:
+        pod = pod_class.Pods(metric['metric']['pod'])
+        pod_index = pod_list_funcs.get_pod_index(pod_list, pod.pod_name)
+        
+        if pod_index == -1:
+            pod_list.append(pod)
+            pod_index = len(pod_list) - 1
+
+        pod_list[pod_index].set_pod_requests(metric['metric']['resource'], metric['value'][1])
+'''

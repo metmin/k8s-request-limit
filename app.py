4d581fd52f34a8cc3056362e299b9f078e7b5db6
Mehmet Emin Bora <mehmet.bora@trendyol.com>
2021-08-09 00:02:31 +0300

    upgraded

--- app.py
+++ app.py
@@ -13,13 +13,12 @@ prometheus_api.get_requests_from_prometheus(pod_list)
 prometheus_api.get_limits_from_prometheus(pod_list)
 prometheus_api.get_cpu_usage_from_prometheus(pod_list)
 
-"""
+
 for pod in pod_list:
   print(pod.pod_name)
-  print(pod.cpu_req + " => " + pod.cpu_limit)
+  print(pod.cpu_req + " => " + pod.cpu_limit + " => " + pod.cpu_usage)
   print(pod.mem_req + " => " + pod.mem_limit)
   print("-----")
-"""
 
 
 '''
--- prometheus_api.py
+++ prometheus_api.py
@@ -36,10 +36,11 @@ def get_limits_from_prometheus(pod_list):
 
         pod_list[pod_index].set_pod_limits(metric['metric']['resource'], metric['value'][1])
 
+
 def get_cpu_usage_from_prometheus(pod_list):
     metrics = get_data('http://prometheus-server:80', 'sum(irate(container_cpu_usage_seconds_total{container!=""}[2m]))by(node,pod)')
     print(metrics)
-"""
+
     for metric in metrics:
         pod_index = pod_list_funcs.get_pod_index(pod_list, metric['metric']['pod'])
         
@@ -48,5 +49,4 @@ def get_cpu_usage_from_prometheus(pod_list):
             pod_list.append(pod)
             pod_index = len(pod_list) - 1
 
-        pod_list[pod_index].set_pod_limits(metric['metric']['resource'], metric['value'][1])
-"""
+        pod_list[pod_index].cpu_usage = metric['value'][1]

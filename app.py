b3359aaef1516db50a23031fc41398d15afb4344
Mehmet Emin Bora <mehmet.bora@trendyol.com>
2021-08-08 19:11:53 +0300

    upgraded

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-import requests 
+import requests
+import pod_class
 
 PROMETHEUS = 'http://prometheus-server:80' #conf dosyasından çekilecek.
 
@@ -6,36 +7,9 @@ PROMETHEUS = 'http://prometheus-server:80' #conf dosyasından çekilecek.
 # TODO: cluster name (ör: p-checkout-p1-2moon) conf'a eklenecek ileride gitlab pipeline'ından verilecek. Amaç: slack'e mesaj gönderirken kullanılacak.
 # TODO: slack entegrasyonu
 
-class Pods:
-  def __init__(self, pod_name, cluster = 'deneme'):
-  #def __init__(self, pod, cluster, cpu_req, cpu_limit, cpu_usage, mem_req, mem_limit, mem_usage):
-      self.pod_name = pod_name
-      self.cluster = cluster
-      '''
-      self.cpu_req = cpu_req
-      self.cpu_limit = cpu_limit
-      self.cpu_usage = cpu_usage
-      self.mem_req = mem_req
-      self.mem_limit = mem_limit
-      self.mem_usage = mem_usage
-      '''
-  
-  # Prometheus sorgularından gelen cevaplar ayrı ayrı listeye kaydedilmesi için farklı fonksiyonlar ayarlandı.
-  def set_pod_requests(self, cpu_req, mem_req):
-      self.cpu_req = cpu_req
-      self.mem_req = mem_req
-
-  def set_pod_limits(self, cpu_limit, mem_limit):
-      self.cpu_limit = cpu_limit
-      self.mem_limit = mem_limit
-  
-  def set_pod_cpu_usage(self, cpu_usage):
-      self.cpu_usage = cpu_usage
-  
-  def set_pod_memory_usage(self, mem_usage):
-      self.mem_usage = mem_usage
 
 pod_list = []
+
 def get_pod_index(pod_name):
   index = 0
   for pod in pod_list:
@@ -45,6 +19,7 @@ def get_pod_index(pod_name):
     index = index + 1
     
   return -1
+
 
 
 
@@ -59,7 +34,7 @@ metrics = get_data('http://prometheus-server:80', 'kube_pod_container_resource_r
 
 for metric in metrics:
 
-  pod = Pods(metric['metric']['pod'])
+  pod = pod_class.Pods(metric['metric']['pod'])
   pod_index = get_pod_index(pod.pod_name)
   
   if pod_index == -1:

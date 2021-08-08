fb6b2ff4356dcf41f4aafca5f1237bad49f1efef
Mehmet Emin Bora <mehmet.bora@trendyol.com>
2021-08-08 19:21:12 +0300

    upgraded

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import prometheus_api
-import pod_class
 
 PROMETHEUS = 'http://prometheus-server:80' #conf dosyasından çekilecek.
 
@@ -10,35 +9,7 @@ PROMETHEUS = 'http://prometheus-server:80' #conf dosyasından çekilecek.
 
 pod_list = []
 
-def get_pod_index(pod_name):
-  index = 0
-  for pod in pod_list:
-    if pod.pod_name == pod_name:
-      # print(pod.pod_name + " => " + pod_name)
-      return index
-    index = index + 1
-    
-  return -1
-
-
-
-metrics = prometheus_api.get_data('http://prometheus-server:80', 'kube_pod_container_resource_requests')
-
-
-for metric in metrics:
-
-  pod = pod_class.Pods(metric['metric']['pod'])
-  pod_index = get_pod_index(pod.pod_name)
-  
-  if pod_index == -1:
-    pod_list.append(pod)
-    pod_index = len(pod_list) - 1
-
-  if metric['metric']['resource'] == 'cpu':
-    pod_list[pod_index].cpu_req = metric['value'][1]
-  elif metric['metric']['resource'] == 'memory':
-    pod_list[pod_index].mem_req = metric['value'][1]
-
+prometheus_api.set_pod_requests(pod_list)
 
 for pod in pod_list:
   print(pod.pod_name)
--- prometheus_api.py
+++ prometheus_api.py
@@ -1,7 +1,26 @@
 import requests
+import pod_class
+import pod_funcs
 
 def get_data(prometheus_url, query):
   response = requests.get(prometheus_url + '/api/v1/query', params={'query': query})
   results = response.json()['data']['result']
 
   return results
+
+
+def set_pod_requests(pod_list):
+    metrics = get_data('http://prometheus-server:80', 'kube_pod_container_resource_requests')
+
+    for metric in metrics:
+        pod = pod_class.Pods(metric['metric']['pod'])
+        pod_index = pod_funcs.get_pod_index(pod.pod_name)
+        
+        if pod_index == -1:
+            pod_list.append(pod)
+            pod_index = len(pod_list) - 1
+
+        if metric['metric']['resource'] == 'cpu':
+            pod_list[pod_index].cpu_req = metric['value'][1]
+        elif metric['metric']['resource'] == 'memory':
+            pod_list[pod_index].mem_req = metric['value'][1]

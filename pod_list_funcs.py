6a8e4a689139ce10626ef76785701d445e03a963
Mehmet Emin Bora <mehmet.bora@trendyol.com>
2021-08-09 11:52:41 +0300

    upgraded

--- pod_list_funcs.py
+++ pod_list_funcs.py
@@ -21,10 +21,15 @@ def calculate_cpu_diff(pod_list):
     cpu_usage = float(pod.cpu_usage)
     mem_req   = int(pod.mem_req)   / 1024 / 1024
     mem_usage = int(pod.mem_usage) / 1024 / 1024
+  
+    cpu_diff =  abs(cpu_usage - cpu_req) / cpu_usage * 100    
+    mem_diff =  abs(mem_usage - mem_req) / mem_usage * 100    
+
+    # abs(usage - request) / usage * 100 
 
     print(pod.pod_name)
-    print(f"{cpu_req} => {cpu_usage}")
-    print(f"{mem_req} => {mem_usage}")
+    print(f"{cpu_req} => {cpu_usage} => {cpu_diff}")
+    print(f"{mem_req} => {mem_usage} => {mem_diff}")
     print("-----")
 
   return 0

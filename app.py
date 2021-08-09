eb6f22e81e1450fe3d52f2415e3290136bc12b11
Mehmet Emin Bora <mehmet.bora@trendyol.com>
2021-08-09 11:13:53 +0300

    upgraded

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import prometheus_api
+import pod_list_funcs
 
 PROMETHEUS = 'http://prometheus-server:80' #conf dosyasından çekilecek.
 
@@ -13,13 +14,15 @@ prometheus_api.get_requests_from_prometheus(pod_list, PROMETHEUS)
 prometheus_api.get_limits_from_prometheus(pod_list, PROMETHEUS)
 prometheus_api.get_cpu_usage_from_prometheus(pod_list, PROMETHEUS)
 prometheus_api.get_memory_usage_from_prometheus(pod_list, PROMETHEUS)
+pod_list_funcs.calculate_cpu_diff(pod_list)
 
-
+"""
 for pod in pod_list:
   print(pod.pod_name)
   print(pod.cpu_req + " => " + pod.cpu_limit + " => " + pod.cpu_usage)
   print(pod.mem_req + " => " + pod.mem_limit + " => " + pod.mem_usage)
   print("-----")
+"""
 
 
 '''
--- pod_list_funcs.py
+++ pod_list_funcs.py
@@ -7,3 +7,24 @@ def get_pod_index(pod_list, pod_name):
     index = index + 1
     
   return -1
+
+
+def calculate_cpu_diff(pod_list):
+  
+  for pod in pod_list:
+
+    if pod.cpu_req == "" or pod.mem_req == "":
+      # TODO: Slack kanalına ayarlanmadığı ile ilgili mesaj göndersin.
+      return -1
+
+    cpu_req   = float(pod.cpu_req) 
+    cpu_usage = float(pod.cpu_usage)
+    mem_req   = int(pod.mem_req)   / 1024 / 1024
+    mem_usage = int(pod.mem_usage) / 1024 / 1024
+
+    print(pod.pod_name)
+    print(cpu_req + " => " + cpu_usage)
+    print(mem_req + " => " + mem_usage)
+    print("-----")
+
+  return 0

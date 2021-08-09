8d6255f536492edb9de4135f98c59bc48473792c
Mehmet Emin Bora <mehmet.bora@trendyol.com>
2021-08-09 15:24:19 +0300

    upgraded

--- slack_api.py
+++ slack_api.py
@@ -4,16 +4,9 @@ from slack_sdk.webhook import WebhookClient
 
 def send_notification():
     url = "https://hooks.slack.com/services/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"
+    
     webhook = WebhookClient(url)
-    response = webhook.send(
-        text="Deneme",
-        blocks=[
-            {
-                "type": "section",
-                "text": {
-                    "type": "mrkdwn",
-                    "text": "You have a new request:\n*<fakeLink.toEmployeeProfile.com|Fred Enriquez - New device request>*"
-                }
-            }
-        ]
-    )
+
+    response = webhook.send(text="Hello!")
+    assert response.status_code == 200
+    assert response.body == "ok"

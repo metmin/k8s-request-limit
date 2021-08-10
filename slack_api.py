9985c17b22cea33d1c33640937a90011f71f7747
Mehmet Emin Bora <mehmet.bora@trendyol.com>
2021-08-10 09:41:10 +0300

    upgraded

--- slack_api.py
+++ slack_api.py
@@ -2,33 +2,27 @@ from slack_sdk.webhook import WebhookClient
 
 # TODO: webhook URL configden gelicek.
 
-def send_diff_notification(webhook_url, pod, cpu_diff, mem_diff):
+def send_notification(notification_type, webhook_url, pod, cpu_diff = "", mem_diff = ""):
+    
+    if notification_type == "DIFF":
+        title = "Request Usage Difference Detected"
+        message = f"Cluster Name: {pod.cluster}\nPod Name: {pod.pod_name}\nCPU Request: {pod.cpu_req}\nCPU Usage: {pod.cpu_usage}\nCPU Diff: %{cpu_diff}\nMem Request: {pod.cpu_req}\nMem Usage: {pod.cpu_usage}\nMem Diff: %{mem_diff}"
+    elif notification_type == "ERROR":
+        title = "Error Detected"
+        message = f"There is an error about {pod.cluster}/{pod.pod_name} cpu or memory request"
+    else:
+        return 0
+
     webhook = WebhookClient(webhook_url)
 
     response = webhook.send(
-        text="Request Usage Difference Detected",
+        text=title,
         blocks=[
             {
                 "type": "section",
                 "text": {
                     "type": "mrkdwn",
-                    "text": f"Cluster Name: {pod.cluster}\nPod Name: {pod.pod_name}\nCPU Request: {pod.cpu_req}\nCPU Usage: {pod.cpu_usage}\nCPU Diff: %{cpu_diff}\nMem Request: {pod.cpu_req}\nMem Usage: {pod.cpu_usage}\nMem Diff: %{mem_diff}"
-                }
-            }
-        ]
-    )
-
-def send_error_notification(webhook_url, pod):
-    webhook = WebhookClient(webhook_url)
-
-    response = webhook.send(
-        text="Request Usage Difference Detected",
-        blocks=[
-            {
-                "type": "section",
-                "text": {
-                    "type": "mrkdwn",
-                    "text": f"There is an error about {pod.cluster}/{pod.pod_name} cpu or memory request"
+                    "text": message
                 }
             }
         ]

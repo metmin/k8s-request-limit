82fdc0ca86dc80c0bb504fc32b638fa342f224ba
Mehmet Emin Bora <mehmet.bora@trendyol.com>
2021-08-11 13:14:49 +0300

    upgraded

--- src/pod_list_funcs.py
+++ src/pod_list_funcs.py
@@ -12,13 +12,19 @@ def get_pod_index(pod_list, pod_name):
 
 
 def calculate_diff(pod_list):
-  
+    
+    notification_list = {
+        "ERROR": [],
+        "DIFF": [],
+    }
+
     for pod in pod_list:
 
         if pod.cpu_req == "" or pod.mem_req == "" or pod.cpu_usage == "0" or pod.cpu_usage == "":
             # TODO: Slack kanalına ayarlanmadığı ile ilgili mesaj göndersin. - DONE
             # TODO: cpu kullanımı 0'sa da mesaj basabilir. 
-            _ = slack_api.send_notification("ERROR", config.WEBHOOK_URL, pod)
+            notification_list["ERROR"].append(pod)
+            #_ = slack_api.send_notification("ERROR", config.WEBHOOK_URL, pod)
             continue
 
         cpu_req   = float(pod.cpu_req)
@@ -31,5 +37,7 @@ def calculate_diff(pod_list):
         mem_diff =  abs(mem_usage - mem_req) / mem_usage * 100
 
         if cpu_diff > 10000 or mem_diff > 150:
-            _ = slack_api.send_notification("DIFF", config.WEBHOOK_URL, pod, cpu_diff, mem_diff)
-        
+            #_ = slack_api.send_notification("DIFF", config.WEBHOOK_URL, pod, cpu_diff, mem_diff)
+            notification_list["DIFF"].append({'pod': pod, 'cpu_diff': cpu_diff, 'mem_diff': mem_diff})
+    
+    print(notification_list)

60fa1307ed5d81b66a3b213c04a253197d258579
Mehmet Emin Bora <mehmet.bora@trendyol.com>
2021-08-12 15:42:35 +0300

    updated

--- src/app.py
+++ src/app.py
@@ -8,18 +8,18 @@ import config
 
 for prometheus_url in config.PROMETHEUS:
   pod_list = []
-  #diff_message = ''
-  #error_message = ''
-  #query = prometheus_api.get_ignored_namespaces_query()
-  #prometheus_api.get_requests_from_prometheus(pod_list, prometheus_url, query)
-  #prometheus_api.get_limits_from_prometheus(pod_list, prometheus_url, query)
-  #prometheus_api.get_cpu_usage_from_prometheus(pod_list, prometheus_url, query)
-  #prometheus_api.get_memory_usage_from_prometheus(pod_list, prometheus_url, query)
+  diff_message = ''
+  error_message = ''
+  query = prometheus_api.get_ignored_namespaces_query()
+  prometheus_api.get_requests_from_prometheus(pod_list, prometheus_url, query)
+  prometheus_api.get_limits_from_prometheus(pod_list, prometheus_url, query)
+  prometheus_api.get_cpu_usage_from_prometheus(pod_list, prometheus_url, query)
+  prometheus_api.get_memory_usage_from_prometheus(pod_list, prometheus_url, query)
 
-  #diff_message, error_message = pod_list_funcs.calculate_diff(pod_list)
+  diff_message, error_message = pod_list_funcs.calculate_diff(pod_list)
 
-print(config.WEBHOOK_URL)
-_, _ = slack_api.send_notification(config.WEBHOOK_URL, 'asdasd', 'jgfdikfsdp')
+  _, _ = slack_api.send_notification(config.WEBHOOK_URL, diff_message, error_message)
+
 
 '''
 git add . ; git commit -m "upgraded" ; git push
--- src/pod_list_funcs.py
+++ src/pod_list_funcs.py
@@ -8,13 +8,12 @@ def get_pod_index(pod_list, pod_name, cluster_name, team_name):
 
 
 def calculate_diff(pod_list):
-    
     diff_message = ""
     error_message = ""
 
     for pod in pod_list:
         if pod.cpu_req == "" or pod.mem_req == "" or pod.cpu_usage == "0" or pod.cpu_usage == "":
-            error_message += f"```There is an error about {pod.cluster}/{pod.pod_name} cpu or memory request```\n"
+            error_message += f"```There is an error about {pod.team_name}/{pod.cluster_name}/{pod.pod_name} cpu or memory request```\n"
             continue
 
         cpu_req   = float(pod.cpu_req)
@@ -26,8 +25,7 @@ def calculate_diff(pod_list):
         cpu_diff =  abs(cpu_usage - cpu_req) / cpu_usage * 100
         mem_diff =  abs(mem_usage - mem_req) / mem_usage * 100
 
-        if cpu_diff > 10000 or mem_diff > 150:
+        if cpu_diff > 20 or mem_diff > 20:
             diff_message += f"```Team Name: {pod.team_name}\nCluster Name: {pod.cluster_name}\n\tPod Name: {pod.pod_name}\n\t\tCPU Request: {pod.cpu_req}\n\t\tCPU Usage: {pod.cpu_usage}\n\t\tCPU Diff: %{cpu_diff}\n\t\t--------------------\n\t\tMem Request: {pod.mem_req}\n\t\tMem Usage: {pod.mem_usage}\n\t\tMem Diff: %{mem_diff}```\n"
-    
-    print(diff_message)
+
     return diff_message, error_message
